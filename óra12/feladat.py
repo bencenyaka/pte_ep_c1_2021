0e40bfe436b93c0fc012805289a993be30de2fc4
bencenyaka <nyakabence2003@gmail.com>
2021-12-03 12:14:09 +0100

    Külön program.

--- óra12/feladat.py
+++ óra12/feladat.py
@@ -1,6 +1,7 @@
 def menu_opciok():
     print("Válasszon az alábbi menüpontok közül\n\t0 - Kilépés"
-          "\n\t1 - új fa rögzítése")
+          "\n\t1 - új fa rögzítése"
+          "\n\t2 - Fajta bekérés")
 
 def egesz_szam_bekerese(koordinata: str):
     szam = ""
@@ -33,6 +34,24 @@ def szotar_kiiratasa(szotar, filepath):
         fileobject.write("\n")
     fileobject.close()
 
+def szotar_betoltes(filepath: str):
+    szotar = {}
+    file = open(filepath, 'r')
+    for sor in file:
+        adat = sor.strip().split('\t')
+        szotar[int(adat[0]), int(adat[1])] = adat[2]
+    file.close()
+    return szotar
+
+def fafajta_lekerdezese(fak):
+    x = egesz_szam_bekerese("x")
+    y = egesz_szam_bekerese("y")
+    hely = (x,y)
+    if hely in fak:
+        print(fak[hely])
+    else:
+        print("Ezen a koordinátán nincs fa.")
+
 menu= ""
 fak_szotar_filepath='fak.csv'
 fak = {}
@@ -41,4 +60,6 @@ while menu!="0":
     menu = input()
     if menu == "1":
         fa_regisztralasa(fak)
+    elif menu == "2":
+        fafajta_lekerdezese(fak)
 szotar_kiiratasa(fak, fak_szotar_filepath)
